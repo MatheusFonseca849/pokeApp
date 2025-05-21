5e51ce3a394361e8bcae9ddcc983ae7b76e2b6f5
matheusOutsmart <matheus@outsmart.com.br>
2025-05-21 18:17:18 -0300

    feat: dark-mode switch added to all drawer screens

--- src/navigation/DrawerRoutes.jsx
+++ src/navigation/DrawerRoutes.jsx
@@ -2,51 +2,103 @@ import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { Ionicons } from "@expo/vector-icons";
 import { FavoritesStackScreen, HomeStackScreen, ItemsStackScreen, TeamsStackScreen } from './StackRoutes';
+import { useContext } from 'react';
+import { ThemeContext } from '../providers/ThemeContext';
+import { IconButton } from 'react-native-paper';
 
 const Drawer = createDrawerNavigator()
 
 export default function DrawerRoutes() {
+  const { theme } = useContext(ThemeContext);
   return (
     <Drawer.Navigator>
       <Drawer.Screen
-      name='Home'
-      component={HomeStackScreen}
-      options={{
-          title: "Início",
-          drawerIcon: (color, size) => (
-            <Ionicons name="home" color={"#4264a8"} size={size} />
-          ),
-        }}
-      />
+    name="Home"
+    component={HomeStackScreen}
+    options={({ navigation }) => {
+      const { toggleTheme, isDarkMode } = useContext(ThemeContext);
+
+      return {
+        title: "Início",
+        drawerIcon: ({ color, size }) => (
+          <Ionicons name="home" color={color} size={size} />
+        ),
+        headerRight: () => (
+          <IconButton
+            icon={isDarkMode ? "white-balance-sunny" : "moon-waxing-crescent"}
+            size={24}
+            onPress={toggleTheme}
+            accessibilityLabel="Toggle dark/light mode"
+          />
+        ),
+      };
+    }}
+  />
       <Drawer.Screen
       name='Favorites'
       component={FavoritesStackScreen}
-      options={{
+      options={({ navigation }) => {
+        const { toggleTheme, isDarkMode } = useContext(ThemeContext);
+
+        return {
           headerTitle: "Favoritos",
           drawerIcon: (color, size) => (
-            <Ionicons name="heart" color={"#4264a8"} size={size} />
+            <Ionicons name="heart" color={color} size={size} />
           ),
-        }}
+          headerRight: () => (
+            <IconButton
+              icon={isDarkMode ? "white-balance-sunny" : "moon-waxing-crescent"}
+              size={24}
+              onPress={toggleTheme}
+              accessibilityLabel="Toggle dark/light mode"
+            />
+          ),
+        };
+      }}
       />
       <Drawer.Screen
       name='Items'
       component={ItemsStackScreen}
-      options={{
+      options={({ navigation }) => {
+        const { toggleTheme, isDarkMode } = useContext(ThemeContext);
+
+        return {
           headerTitle: "Itens",
           drawerIcon: (color, size) => (
-            <Ionicons name="heart" color={"#4264a8"} size={size} />
+            <Ionicons name="heart" color={color} size={size} />
           ),
-        }}
+          headerRight: () => (
+            <IconButton
+              icon={isDarkMode ? "white-balance-sunny" : "moon-waxing-crescent"}
+              size={24}
+              onPress={toggleTheme}
+              accessibilityLabel="Toggle dark/light mode"
+            />
+          ),
+        };
+      }}
       />
       <Drawer.Screen
       name='Teams'
       component={TeamsStackScreen}
-      options={{
+      options={({ navigation }) => {
+        const { toggleTheme, isDarkMode } = useContext(ThemeContext);
+
+        return {
           headerTitle: "Times",
           drawerIcon: (color, size) => (
-            <Ionicons name="heart" color={"#4264a8"} size={size} />
+            <Ionicons name="heart" color={color} size={size} />
           ),
-        }}
+          headerRight: () => (
+            <IconButton
+              icon={isDarkMode ? "white-balance-sunny" : "moon-waxing-crescent"}
+              size={24}
+              onPress={toggleTheme}
+              accessibilityLabel="Toggle dark/light mode"
+            />
+          ),
+        };
+      }}
       />
     </Drawer.Navigator>
   )
